90664bf0771752ba9aca6584605259cc8a4e262c
nhelfman <noamh@microsoft.com>
2022-01-30 12:14:22 +0200

    add memory consumption field

--- worker.js
+++ worker.js
@@ -1,6 +1,15 @@
+let buffer = [];
+
 onmessage = function(e) {
     const workerId = e.data[0];
-    console.log('Message received from main script: ' + workerId);
- 
+    const consumeMB = e.data[1];
+
+    console.log('Message received from main script: ' + workerId, consumeMB);
+    
+    // buffer = new Int8Array(1024*1024*consumeMB);
+    for(let i=0; i < consumeMB * 1024 * 1024 / 8 / 1.4; i++) {
+        buffer.push(1);
+    };
+
     postMessage(['worker ' + workerId]);
-  }
+}
